ec0a4be8bd1eb2a13238a68e2d788bc6a67f22ff
Cody Markham <cgmarkham64@gmail.com>
2024-04-21 22:29:38 -0600

    Adjusting for flex boxes and such, WIP

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,66 +4,88 @@ import './Home.scss'
 import React, { Component } from 'react'
 
 class Home extends Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    // Set initial state
-    this.state = {
-      titleText: 'Hello,',
-      leftPanelText:
-        "I'm Cody Markham and I’m a full stack software engineer, outdoor enthusiast, and photographer. I currently live in Colorado and, like most folks here, " +
-        "I collect all the hobbies when I'm not working on code and sipping mochas. You can find me hiking in the summer and skiing in the winter, usually with a " +
-        'camera in hand. This website is a place where I can showcase my skills and play with different front end techniques and ideas. Anyway, that’s enough of an ' +
-        'intro for a home page, check out my About page for a digitized version of my resume, check out my Photography page if you came here to see some of my art, ' +
-        'and hit me up via my contact page just to see if I got my email linkages correct! ',
-      instagramElements: [],
-    };
+        // Set initial state
+        this.state = {
+            titleText: 'Hello,',
+            leftPanelText:
+                "I'm Cody Markham and I’m a full stack software engineer, outdoor enthusiast, and photographer. I currently live in Colorado and, like most folks here, " +
+                "I collect all the hobbies when I'm not working on code and sipping mochas. You can find me hiking in the summer and skiing in the winter, usually with a " +
+                'camera in hand. This website is a place where I can showcase my skills and play with different front end techniques and ideas. Anyway, that’s enough of an ' +
+                'intro for a home page, check out my About page for a digitized version of my resume, check out my Photography page if you came here to see some of my art, ' +
+                'and hit me up via my contact page just to see if I got my email linkages correct!',
+            instagramElements: [],
+        }
 
-    this.getUserMedia = this.getUserMedia.bind(this);
-  }
+        this.getUserMedia = this.getUserMedia.bind(this)
+    }
 
-  componentDidMount() {
-    this.getUserMedia();
-  }
+    componentDidMount() {
+        this.getUserMedia()
+    }
 
-  getUserMedia = async () => {
-    const accessToken = process.env.REACT_APP_INSTAGRAM_KEY;
-    const fields = `id,caption,media_url,timestamp,media_type,permalink`;
-    const userId = '7436680339745871';
-    const url = `https://graph.instagram.com/v19.0/${userId}/media?fields=${fields}&access_token=${accessToken}`;
-    const data = await fetch(url);
-    const feed = await data.json();
+    getUserMedia = async () => {
+        const accessToken = process.env.REACT_APP_INSTAGRAM_KEY
+        const fields = `id,caption,media_url,timestamp,media_type,permalink`
+        const userId = '7436680339745871'
+        const url = `https://graph.instagram.com/v19.0/${userId}/media?fields=${fields}&access_token=${accessToken}`
+        const data = await fetch(url)
+        const feed = await data.json()
 
-    console.debug(feed);
+        console.debug(feed)
 
-    let elements = [];
+        let elements = []
 
-    feed.data.forEach((item) => {
-      elements.push(<InstaItem key={item.id} item={item}></InstaItem>)
-    });
+        feed.data.forEach((item, index) => {
+            if (item.media_type !== 'VIDEO') {
+                elements.push(
+                    <div className="scroll-element">
+                        <InstaItem key={item.id} item={item}></InstaItem>
+                    </div>
+                )
+            }
+        });
 
-    this.setState({
-      instagramElements: elements,
-    });
-  }
+        this.setState({
+            instagramElements: elements,
+        });
+    }
 
-  render() {
-    return (
-      <div className="container">
-        <div className="home-page">
-          <div className="content-zone left-zone">
-            <h1>{this.state.titleText}</h1>
-            <h3>{this.state.leftPanelText}</h3>
-            <h3>See you on the trail!</h3>
-            <h3>—Cody</h3>
-          </div>
-          <div className="content-zone right-zone">
-            {this.state.instagramElements}
-          </div>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="container">
+                <div className="home-page">
+                    <div className="left-container">
+                        <div className="content-zone zone-one">
+                            <h1>{this.state.titleText}</h1>
+                            <h3>{this.state.leftPanelText}</h3>
+                            <h3>See you on the trail!</h3>
+                            <h3>—Cody</h3>
+                        </div>
+                        <div className="content-zone zone-two">
+                            {/* TODO side scroll https://www.youtube.com/watch?v=iLmBy-HKIAw&t=1s */}
+                            <ul>
+                                <li>HTML</li>
+                                <li>JavaScript</li>
+                                <li>ReactJS</li>
+                                <li>Angular</li>
+                                <li>Python</li>
+                                <li>Java</li>
+                                <li>CSS</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="right-container">
+                        <div className="content-zone zone-one">
+                            {this.state.instagramElements}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default Home;
+export default Home
--- src/components/InstaItem/index.js
+++ src/components/InstaItem/index.js
@@ -14,7 +14,6 @@ class InstaItem extends Component {
   }
 
   render() {
-    if (this.state.item.media_type !== 'VIDEO') {
       return (
         <div className="instaItem-container">
           <a href={this.state.item.permalink} target="_blank">
@@ -22,12 +21,12 @@ class InstaItem extends Component {
               src={this.state.item.media_url}
               alt={`insta-id-${this.state.item.id}`}
               className="image"
+              width="650px"
             />
           </a>
           <h4 className="caption">{this.state.item.caption}</h4>
         </div>
       )
-    }
   }
 }
 
